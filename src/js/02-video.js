a0edb57f54f5294033cd61a2f8d2818299d4686b
ViacheslavDedov <dedoff.v@gmail.com>
2022-05-09 14:01:54 +0300

    DZ 8/2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onPlay,1000) );
+ 
+ function onPlay(timeupdate) {
+    const time = timeupdate.seconds;
+localStorage.setItem('videoplayer-current-time', `${time}`);
+    
+};
+
+const getTime = localStorage.getItem('videoplayer-current-time');
+
+let newTime = Number(getTime);
+
+player.setCurrentTime(newTime).then(function(seconds) {
+
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
