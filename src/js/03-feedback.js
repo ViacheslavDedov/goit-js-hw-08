15009462f95d2315989b809925ab1abde84a8f37
ViacheslavDedov <dedoff.v@gmail.com>
2022-05-13 11:07:33 +0300

    dz 8/3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+populateText();
+
+function onInput(event) {
+    const { name, value } = event.target;
+    const formData = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+    formData[name] = value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+};
+
+function populateText() {
+    const getMessage = localStorage.getItem("feedback-form-state");
+
+    if (getMessage) {
+        const getObject = JSON.parse(getMessage);
+        if (getObject) {
+            Object.keys(getObject).forEach((key) => {
+                refs.form.elements[key].value = getObject[key];
+            });
+        }
+    }
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(refs.form);
+    formData.forEach((value, key) => {
+        console.log(key, value);
+    });
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+};
